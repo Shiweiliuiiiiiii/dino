aac7defee2ea3c39efa3a8f14d649ecc22444919
Shiweiliuiiiiiii <s.liu3@tue.nl>
2022-07-03 18:16:09 +0200

    add slak

--- main_dino.py
+++ main_dino.py
@@ -60,7 +60,7 @@ def get_args_parser():
 
     # Model parameters
     parser.add_argument('--arch', default='vit_small', type=str,
-        choices=['vit_tiny', 'vit_small', 'vit_base', 'xcit', 'deit_tiny', 'deit_small'] \
+        choices=['vit_tiny', 'vit_small', 'vit_base', 'xcit', 'deit_tiny', 'deit_small', 'SLaK_tiny', 'SLaK_small', 'SLaK_base', 'SLaK_large'] \
                 + torchvision_archs + torch.hub.list("facebookresearch/xcit:main"),
         help="""Name of architecture to train. For quick experiments with ViTs,
         we recommend using vit_tiny or vit_small.""")
