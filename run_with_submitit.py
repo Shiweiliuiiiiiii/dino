2564ec4588b56d28d4fd43af4df96b54f9a2ec7f
刘世伟 <liushiwei@v078120.vpn.tue.nl>
2022-10-06 12:28:26 -0500

    slak small

--- run_with_submitit.py
+++ run_with_submitit.py
@@ -108,7 +108,7 @@ def main():
         kwargs['slurm_comment'] = args.comment
 
     executor.update_parameters(
-        mem_gb=40 * num_gpus_per_node * nodes,
+        # mem_gb=40 * num_gpus_per_node * nodes,
         gpus_per_node=num_gpus_per_node,
         tasks_per_node=num_gpus_per_node,  # one task per GPU
         cpus_per_task=18,
--- scripts/slak_small_9_bn_300.sh
+++ scripts/slak_small_9_bn_300.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 source /home/sliu/miniconda3/etc/profile.d/conda.sh
-source activate slak
+source activate slak4
 
-python run_with_submitit.py --nodes 2 --ngpus 4 --bn True --teacher_temp 0.07 --warmup_teacher_temp_epochs 30 --norm_last_layer false \
+python run_with_submitit.py --nodes 2 --ngpus 4 --bn False --teacher_temp 0.07 --warmup_teacher_temp_epochs 30 --norm_last_layer false \
 --arch SLaK_tiny --kernel_size 9 9 9 9 100 --epochs 300 --batch_size_per_gpu 128 --timeout 60  \
 --data_path /projects/2/managed_datasets/imagenet/train --output_dir /projects/0/prjste21060/projects/dino/dino_slak_small_9_bn_300/
 
