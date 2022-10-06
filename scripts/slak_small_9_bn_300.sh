235b63a5bb60acbb7765aafedaab3e12fabe1b0a
刘世伟 <liushiwei@v078120.vpn.tue.nl>
2022-10-06 13:18:40 -0500

    solve nan loss

--- scripts/slak_small_9_bn_300.sh
+++ scripts/slak_small_9_bn_300.sh
@@ -4,7 +4,7 @@ source /home/sliu/miniconda3/etc/profile.d/conda.sh
 source activate slak
 
 python run_with_submitit.py --nodes 2 --ngpus 4 --bn False --teacher_temp 0.07 --warmup_teacher_temp_epochs 30 --norm_last_layer false \
---arch SLaK_small --kernel_size 9 9 9 9 100 --epochs 300 --batch_size_per_gpu 64 --timeout 60 --use_fp16 False \
+--arch SLaK_small --kernel_size 9 9 9 9 100 --epochs 300 --batch_size_per_gpu 32 --timeout 60 --use_fp16 False \
 --data_path /projects/2/managed_datasets/imagenet/train --output_dir /projects/0/prjste21060/projects/dino/dino_slak_small_9_bn_300/
 
 source deactivate
