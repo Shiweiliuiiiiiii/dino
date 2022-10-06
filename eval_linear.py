c6aa0ca8791dd7cd79baf56ab6732223205c6fcd
刘世伟 <liushiwei@v078120.vpn.tue.nl>
2022-10-06 16:34:19 -0500

    eval linear

--- eval_linear.py
+++ eval_linear.py
@@ -27,6 +27,19 @@ import models.SLaK
 import utils
 import vision_transformer as vits
 
+def str2bool(v):
+    """
+    Converts string to bool type; enables command line
+    arguments in the format of '--arg1 true --arg2 false'
+    """
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 def eval_linear(args):
     utils.init_distributed_mode(args)
