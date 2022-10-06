0ccb2a224c3d3889c81653893cc5df54419d3e65
刘世伟 <liushiwei@v078120.vpn.tue.nl>
2022-10-06 12:29:58 -0500

    slak small

--- scripts/slak_small_9_bn_300.sh
+++ scripts/slak_small_9_bn_300.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /home/sliu/miniconda3/etc/profile.d/conda.sh
-source activate slak4
+source activate slak
 
 python run_with_submitit.py --nodes 2 --ngpus 4 --bn False --teacher_temp 0.07 --warmup_teacher_temp_epochs 30 --norm_last_layer false \
 --arch SLaK_tiny --kernel_size 9 9 9 9 100 --epochs 300 --batch_size_per_gpu 128 --timeout 60  \
