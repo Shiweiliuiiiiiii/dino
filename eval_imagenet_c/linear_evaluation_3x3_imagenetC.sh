c6b3164ec8bf77f7389e592200f4b2f1c14cedbd
Shiwei Liu <shiwei@v078041.vpn.tue.nl>
2022-11-03 20:53:53 -0500

    rpobustness

--- eval_imagenet_c/linear_evaluation_3x3_imagenetC.sh
+++ eval_imagenet_c/linear_evaluation_3x3_imagenetC.sh
@@ -1,22 +1,7 @@
-#!/bin/bash
-#SBATCH --job-name=linear_slak_3x3
-#SBATCH -p gpu
-#SBATCH -N 1
-#SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-node=1
-#SBATCH --gpus=1
-#SBATCH -t 10:00:00
-#SBATCH --cpus-per-task=18
-#SBATCH -o linear_slak_3x3_S_R_A_300.out
-
-source /home/sliu/miniconda3/etc/profile.d/conda.sh
-source activate slak
-
-ck1=/projects/0/prjste21060/projects/dino/dino_slak_3_bn_300/checkpoint.pth
-ck2=dino_slak_3_bn_300/linear/checkpoint.pth.tar
-python -m torch.distributed.launch --nproc_per_node=1 --master_port=54444   eval_linear.py --evaluate --dataset imagenet_c --data_path data_path \
+ck1=/datadrive_c/ssl/100epochs/dino_slak_3_bn/checkpoint.pth
+ck2=/datadrive_c/ssl/100epochs/dino_slak_3_bn/linear/checkpoint.pth.tar
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=11111   eval_linear.py --evaluate --dataset imagenet_c --data_path data_path \
 --arch SLaK_tiny --kernel_size 3 3 3 3 100 --LoRA False --bn True \
 --pretrained_weights $ck1 --url $ck2 \
---output_dir ./ --checkpoint_key teacher 
+--output_dir ./ --checkpoint_key teacher
 
-source deactivate
