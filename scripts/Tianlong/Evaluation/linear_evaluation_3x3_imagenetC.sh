804e08c3aeed35e86b03881d5ce5726f971b6b68
Shiwei Liu <shiwei@v099080.vpn.tue.nl>
2022-11-04 12:33:29 -0500

    robustness

--- scripts/Tianlong/Evaluation/linear_evaluation_3x3_imagenetC.sh
+++ scripts/Tianlong/Evaluation/linear_evaluation_3x3_imagenetC.sh
@@ -21,19 +21,71 @@ python -m torch.distributed.launch --nproc_per_node=1 --master_port=11111  eval_
 --pretrained_weights $ck1 --url $ck2 \
 --output_dir ./ --checkpoint_key teacher
 
-#python  -m torch.distributed.launch --nproc_per_node=1 --master_port=54444  eval_linear.py --evaluate --data_path /home/sliu/project_space/datasets/sketch \
-#--arch SLaK_tiny --kernel_size 3 3 3 3 100 --LoRA False --bn True \
-#--pretrained_weights $ck1 --url $ck2 \
-#--output_dir ./ --checkpoint_key teacher
 
-#python  -m torch.distributed.launch --nproc_per_node=1 --master_port=54444  eval_linear.py --evaluate --data_path /home/sliu/project_space/datasets/imagenet-r \
-#--arch SLaK_tiny --kernel_size 3 3 3 3 100 --LoRA False --bn True \
-#--pretrained_weights $ck1 --url $ck2 \
-#--output_dir ./ --checkpoint_key teacher
+ck1=/datadrive_c/ssl/100epochs/dino_slak_5_bn/checkpoint.pth
+ck2=/datadrive_c/ssl/100epochs/dino_slak_5_bn/linear/checkpoint.pth.tar
+path=/datadrive_c/Imagenet-C/
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=11112  eval_linear_robustness.py --evaluate --data_path $path --dataset imagenet_c \
+--arch SLaK_tiny --kernel_size 5 5 5 5 100 --LoRA False --bn True \
+--pretrained_weights $ck1 --url $ck2 \
+--output_dir ./ --checkpoint_key teacher
 
-#python  -m torch.distributed.launch --nproc_per_node=1 --master_port=54444  eval_linear.py --evaluate --data_path /home/sliu/project_space/datasets/imagenet-a \
-#--arch SLaK_tiny --kernel_size 3 3 3 3 100 --LoRA False --bn True \
-#--pretrained_weights $ck1 --url $ck2 \
-#--output_dir ./ --checkpoint_key teacher
+
+ck1=/datadrive_c/ssl/100epochs/dino_slak_7_bn/checkpoint.pth
+ck2=/datadrive_c/ssl/100epochs/dino_slak_7_bn/linear/checkpoint.pth.tar
+path=/datadrive_c/Imagenet-C/
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=11113  eval_linear_robustness.py --evaluate --data_path $path --dataset imagenet_c \
+--arch SLaK_tiny --kernel_size 7 7 7 7 100 --LoRA False --bn True \
+--pretrained_weights $ck1 --url $ck2 \
+--output_dir ./ --checkpoint_key teacher
+
+
+ck1=/datadrive_c/ssl/100epochs/dino_slak_9_bn/checkpoint.pth
+ck2=/datadrive_c/ssl/100epochs/dino_slak_9_bn/linear/checkpoint.pth.tar
+path=/datadrive_c/Imagenet-C/
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=11114  eval_linear_robustness.py --evaluate --data_path $path --dataset imagenet_c \
+--arch SLaK_tiny --kernel_size 9 9 9 9 100 --LoRA False --bn True \
+--pretrained_weights $ck1 --url $ck2 \
+--output_dir ./ --checkpoint_key teacher
+
+
+ck1=/datadrive_c/ssl/100epochs/dino_slak_15_bn/checkpoint.pth
+ck2=/datadrive_c/ssl/100epochs/dino_slak_15_bn/linear/checkpoint.pth.tar
+path=/datadrive_c/Imagenet-C/
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=11115  eval_linear_robustness.py --evaluate --data_path $path --dataset imagenet_c \
+--arch SLaK_tiny --kernel_size 15 15 15 15 100 --LoRA False --bn True \
+--pretrained_weights $ck1 --url $ck2 \
+--output_dir ./ --checkpoint_key teacher
+
+
+ck1=/datadrive_c/ssl/100epochs/dino_slak_15_bn/checkpoint.pth
+ck2=/datadrive_c/ssl/100epochs/dino_slak_15_bn/linear/checkpoint.pth.tar
+path=/datadrive_c/Imagenet-C/
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=11116  eval_linear_robustness.py --evaluate --data_path $path --dataset imagenet_c \
+--arch SLaK_tiny --kernel_size 15 15 15 15 100 --LoRA False --bn True \
+--pretrained_weights $ck1 --url $ck2 \
+--output_dir ./ --checkpoint_key teacher
+
+
+ck1=/datadrive_c/ssl/100epochs/dino_swin_tiny/checkpoint.pth
+ck2=/datadrive_c/ssl/100epochs/dino_swin_tiny/linear/checkpoint.pth.tar
+path=/datadrive_c/Imagenet-C/
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=11117  eval_linear_robustness.py --evaluate --data_path $path --dataset imagenet_c \
+--arch swin_tiny \
+--cfg experiments/imagenet/swin/swin_tiny_patch4_window7_224.yaml \
+--pretrained_weights $ck1 --url $ck2 \
+--output_dir ./ --checkpoint_key teacher \
+--batch_size_per_gpu 256 --n_last_blocks 4 --num_labels 1000 MODEL.NUM_CLASSES 0
+
+
+ck1=/datadrive_c/ssl/100epochs/dino_RN50/checkpoint.pth
+ck2=/datadrive_c/ssl/100epochs/dino_RN50/linear/checkpoint.pth.tar
+path=/datadrive_c/Imagenet-C/
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=11118  eval_linear_robustness.py --evaluate --data_path $path --dataset imagenet_c \
+--arch resnet50 \
+--pretrained_weights $ck1 --url $ck2 \
+--output_dir ./ --checkpoint_key teacher \
+--batch_size_per_gpu 256 \
+
 
 source deactivate
