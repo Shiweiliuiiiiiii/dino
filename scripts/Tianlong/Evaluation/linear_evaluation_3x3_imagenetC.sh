43d5ab5de1754d0383d78dd2d9def470ed82279f
Shiwei Liu <shiwei@v078041.vpn.tue.nl>
2022-11-03 20:59:54 -0500

    rpobustness

--- scripts/Tianlong/Evaluation/linear_evaluation_3x3_imagenetC.sh
+++ scripts/Tianlong/Evaluation/linear_evaluation_3x3_imagenetC.sh
@@ -1,6 +1,6 @@
 ck1=/datadrive_c/ssl/100epochs/dino_slak_3_bn/checkpoint.pth
 ck2=/datadrive_c/ssl/100epochs/dino_slak_3_bn/linear/checkpoint.pth.tar
-python -m torch.distributed.launch --nproc_per_node=1 --master_port=11111   eval_linear.py --evaluate --dataset imagenet_c --data_path data_path \
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=11111   eval_linear_robustness.py --evaluate --dataset imagenet_c --data_path data_path \
 --arch SLaK_tiny --kernel_size 3 3 3 3 100 --LoRA False --bn True \
 --pretrained_weights $ck1 --url $ck2 \
 --output_dir ./ --checkpoint_key teacher
