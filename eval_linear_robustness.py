14841c694c9db511020da3b8b65cb9cfca0b059f
Shiwei Liu <shiwei@v078041.vpn.tue.nl>
2022-11-03 21:28:40 -0500

    rpobustness

--- eval_linear_robustness.py
+++ eval_linear_robustness.py
@@ -206,19 +206,19 @@ def eval_linear(args):
     linear_classifier = nn.parallel.DistributedDataParallel(linear_classifier, device_ids=[args.gpu])
 
     # ============ preparing data ... ============
-    val_transform = pth_transforms.Compose([
-        pth_transforms.Resize(256, interpolation=3),
-        pth_transforms.CenterCrop(224),
-        pth_transforms.ToTensor(),
-        pth_transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-    ])
-    dataset_val =ImageFolder(os.path.join(args.data_path, "val"), transform=val_transform)
-    val_loader = torch.utils.data.DataLoader(
-        dataset_val,
-        batch_size=args.batch_size_per_gpu,
-        num_workers=args.num_workers,
-        pin_memory=True,
-    )
+    # val_transform = pth_transforms.Compose([
+    #     pth_transforms.Resize(256, interpolation=3),
+    #     pth_transforms.CenterCrop(224),
+    #     pth_transforms.ToTensor(),
+    #     pth_transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    # ])
+    # dataset_val =ImageFolder(os.path.join(args.data_path, "val"), transform=val_transform)
+    # val_loader = torch.utils.data.DataLoader(
+    #     dataset_val,
+    #     batch_size=args.batch_size_per_gpu,
+    #     num_workers=args.num_workers,
+    #     pin_memory=True,
+    # )
     if args.dataset=='imagenet_c':
         dev_env = initialize_device(force_cpu=False, amp=False)
         print("Evaluate ImageNet C")
