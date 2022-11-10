d4d1091dd6cba7fab3650993c3b3e3ea8f8b8bb5
Shiwei Liu <shiwei@utcsadmins-MacBook-Pro.local>
2022-11-09 19:04:32 -0600

    imagenetc

--- scripts/Tianlong/Evaluation/linear_evaluation_vit_small_imagenetC_300.sh
+++ scripts/Tianlong/Evaluation/linear_evaluation_vit_small_imagenetC_300.sh
@@ -3,7 +3,7 @@
 ck1=/home/shiwei/Projects/dino/ckpts/300epochs/dino_vit_small/checkpoint.pth
 ck2=/home/shiwei/Projects/dino/ckpts/300epochs/dino_vit_small/linear/checkpoint.pth.tar
 path=/home/shiwei/data/
-python -m torch.distributed.launch --nproc_per_node=1 --master_port=11112 eval_linear_robustness.py --evaluate --data_path $path --dataset imagenet_c \
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=10003 eval_linear_robustness.py --evaluate --data_path $path --dataset imagenet_c \
 --arch vit_small --pretrained_weights $ck1 --url $ck2  --output_dir ./ --checkpoint_key teacher \
 
 
